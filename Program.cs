5ab36b95ed9f1d1b6f15d0920cae35fa3e7a17fe
Emanuel Saraiva <emannuel.saraiva@live.com>
2022-04-19 22:19:48 -0300

    atualização primeiro programa

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace _1_PrimeiroProjeto
         static void Main(string[] args)
         {
             Console.WriteLine("Olá, mundo! Meu primeiro programa no visual studio");
+            Console.WriteLine("Nunca pare de estudar");
+
 
             Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
             Console.ReadLine();
